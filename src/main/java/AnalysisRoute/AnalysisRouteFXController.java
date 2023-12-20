ccebd1ba6b0a362e4f68074937d832a2f89ae857
shwang1a <shwang1a@hotmail.com>
2023-12-20 16:31:36 +0800

    update

--- src/main/java/AnalysisRoute/AnalysisRouteFXController.java
+++ src/main/java/AnalysisRoute/AnalysisRouteFXController.java
@@ -7,16 +7,20 @@ import feoverlay.MapManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.util.StringConverter;
 
 public class AnalysisRouteFXController {
 
     private static String[] travelModesOptions = new String[] { "Fastest", "Shortest" };
     @FXML private ComboBox<TravelMode> travelModes;
+    @FXML private ToggleGroup toggleGroup2;
 
     @FXML
     private void initialize() {
@@ -34,6 +38,14 @@ public class AnalysisRouteFXController {
         });
         travelModes.getSelectionModel().selectedItemProperty().addListener(o -> {
             MapManager.mapController.setTravelMode(travelModes.getSelectionModel().getSelectedItem());
+        });
+
+        toggleGroup2.selectedToggleProperty().addListener(o -> {
+            switchTravelMode();               
+        });
+
+        chkHightway.selectedProperty().addListener(o -> {
+            switchTravelMode();               
         });
     }
 
@@ -81,12 +93,78 @@ public class AnalysisRouteFXController {
     
     @FXML
     public void btnSearch_click() {
-    	if (txtSearch.getText().trim() != "")
-    		MapManager.mapController.searchRoad(txtSearch.getText().trim());
+        if (txtSearch.getText().trim() != "")
+            MapManager.mapController.searchRoad(txtSearch.getText().trim());
     }
     
     @FXML
     public void btnClear_click() {
-    	MapManager.mapController.clearSearchResult();
+        MapManager.mapController.clearSearchResult();
     }
+
+    @FXML private RadioButton optSpeed;
+    public boolean optSpeed_isSelected() {
+        return optSpeed.isSelected();
+    }
+    @FXML private RadioButton optBigCar;
+    public boolean optBigCar_isSelected() {
+        return optBigCar.isSelected();
+    }
+    @FXML private RadioButton optSmallCar;
+    public boolean optSmallCar_isSelected() {
+        return optSmallCar.isSelected();
+    }
+    @FXML private RadioButton optMen;
+    public boolean optMen_isSelected() {
+        return optMen.isSelected();
+    }
+    @FXML private TextField txtSpeed;
+    public double getMoveSpeed() {
+        return( Double.valueOf(txtSpeed.getText()) );
+    }
+    @FXML private CheckBox chkHightway;
+    public boolean chkHightway_isSelected() {
+        return chkHightway.isSelected();
+    }
+
+    public void selectTravelModeByName(String travelName)
+    {
+        TravelMode getTravel = null;
+        for(TravelMode travel: travelModes.getItems() )
+        {
+            if( travel.getName().equals(travelName) )
+            {
+                getTravel = travel;
+                break;
+            }
+        }
+        if( getTravel != null )
+            travelModes.getSelectionModel().select(getTravel);
+    }
+
+    // 依目前設定變更分析模式
+    public void switchTravelMode() {
+       if( optMen_isSelected() ) {
+           selectTravelModeByName("行人");
+       }
+       else if( optSpeed_isSelected() ) {
+           if( !chkHightway_isSelected() )
+               selectTravelModeByName("最短距離");
+           else
+               selectTravelModeByName("最短距離_避走高速公路");
+       }
+       else if( optBigCar_isSelected() ) {
+           if( !chkHightway_isSelected() )
+               selectTravelModeByName("大貨車");
+           else
+               selectTravelModeByName("大貨車_避走高速公路");
+       }
+       else if( optSmallCar_isSelected() ) {
+           if( !chkHightway_isSelected() )
+               selectTravelModeByName("小客車");
+           else
+               selectTravelModeByName("小客車_避走高速公路");
+       }
+    }
+
 }
